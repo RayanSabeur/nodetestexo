35b24429dcd1077eda86b0b399a3d43757c3681f
RayanSabeur <85495356+RayanSabeur@users.noreply.github.com>
2023-04-04 10:41:46 +0200

    j2

---
+++ Jour2/index.js
@@ -0,0 +1,10 @@
+const u = require('./util')
+
+
+const priceHT = [
+    { name : "Apple", priceHT : 1.0, priceTTC : null },
+    { name : "Orange", priceHT : 1.2, priceTTC : null },
+    { name : "Rasberry", priceHT : 2.5, priceTTC : null },
+];
+
+u.Utils(priceHT)
---
+++ Jour2/util.js
@@ -0,0 +1,14 @@
+exports.name = "Johns";
+
+exports.Utils = function(name) {
+    
+ 
+  name.map((e) => {
+       e.priceTTC = e.priceHT + 0.8
+    })
+   
+    return name
+
+   
+}
+
